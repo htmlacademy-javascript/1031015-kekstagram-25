fb4034a8116c222a52122a537f6644804a33c40f
Лилия Аникина <liliaanikina@MacBook-Pro-Lilia.local>
2022-04-04 22:36:06 +0300

    full size image, open-close moduls

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import { items } from './data.js';
 import { renderMiniatures } from './rendering_miniatures.js';
+import { openFullSizeImage, closeFullSizeImage } from './drawing_full_size_image';
 // console.log(JSON.stringify(items, null, 4));
 
 renderMiniatures(items);
+openFullSizeImage(items[0]);
+closeFullSizeImage();
+
