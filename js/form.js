66aab38204eed834929afa58fec4b72bfbbde098
Аникина Лилия <zolina.liliya@gmail.com>
2022-04-16 12:26:55 +0300

    module8-task1

--- js/form.js
+++ js/form.js
@@ -1,12 +1,14 @@
 import { isEscapeKey, isInputFocus } from '/js/util.js';
+import { validateForm } from '/js/validator.js';
 
-const imgUploadStart = document.querySelector('.img-upload__label');
+const imgUploadInput = document.querySelector('.img-upload__input');
 const imgUpload = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const imgUploadCancel = document.querySelector('.img-upload__cancel');
 const form = document.querySelector('.img-upload__form');
 const textHashtags = document.querySelector('.text__hashtags');
 const textDescription = document.querySelector('.text__description');
+const imgPreview = document.querySelector('.img-upload__preview');
 
 
 const keyDownEvent = (evt) => {
@@ -14,10 +16,18 @@ const keyDownEvent = (evt) => {
     evt.preventDefault();
     imgUpload.classList.add('hidden');
     body.classList.remove('modal-open');
+    document.removeEventListener('keydown', keyDownEvent);
+    form.reset();
   }
 };
 
-imgUploadStart.addEventListener('click', () => {
+imgUploadInput.addEventListener('change', () => {
+  const file = imgUploadInput.files[0];
+  const element = document.createElement('img');
+
+  element.src = URL.createObjectURL(file);
+  imgPreview.innerHTML='';
+  imgPreview.appendChild(element);
   imgUpload.classList.remove('hidden');
   body.classList.add('modal-open');
 
@@ -31,4 +41,10 @@ imgUploadCancel.addEventListener('click', () => {
   form.reset();
 });
 
+form.addEventListener('submit', (event)=> {
+  const isValid = validateForm();
 
+  if (!isValid) {
+    event.preventDefault();
+  }
+});
--- js/validator.js
+++ js/validator.js
@@ -1,12 +1,41 @@
 const form = document.querySelector('.img-upload__form');
 const pristine = new Pristine(form);
+const hashtagField = document.querySelector('.text__hashtags');
 
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+pristine.addValidator(hashtagField, (value) => {
+  value = value.trim();
 
+  if (value === '') {
+    return true;
+  }
+
+  const hashtags = value.split(' ');
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  let isValid = true;
+
+  const hashtagsList = hashtags.map((item) => item.toLowerCase());
+
+  hashtagsList.forEach((element) => {
+    if (!re.test(element)) {
+      isValid = false;
+    }
+  });
+
+  if (hashtagsList.length > 5) {
+    isValid = false;
+  }
+
+  if (new Set(hashtagsList).size !== hashtagsList.length) {
+    isValid = false;
+  }
+
+  return isValid;
+});
+
+const validateForm = () => {
   const isValid = pristine.validate();
 
-  if (isValid) {
-    // submit form data
-  }
-});
+  return isValid;
+};
+
+export { validateForm };
